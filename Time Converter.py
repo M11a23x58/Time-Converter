c551b4f518cbef20518705fa866ac6141686e75a
M11a23x58 <82143402+M11a23x58@users.noreply.github.com>
2021-04-08 23:24:11 +0800

    Add Time Converter.py
    Enjoy it!

---
+++ Time Converter.py
@@ -0,0 +1,24 @@
+print("This program will convert seconds to hours, minutes and seconds.")
+
+def main():
+# input time
+    time=int(input("Please input the seconds.\n"))
+
+# converter
+    Second = time % 60
+    Minute = ((time - Second) / 60 ) % 60
+    Hour = ((time - Second) / 60 ) / 60
+
+# output time format
+    h="%02d" % Hour
+    m="%02d" % Minute
+    s="%02d" % Second
+    print(h,":",m,":",s)
+
+# exception detect
+while True:
+    try:
+        main()
+    except ValueError:
+        print("Wrong input, try again.\n")
+
