493c94a4ea9f6a3d4f0f5aa2d07cc082f647b7ac
M11a23x58 <82143402+M11a23x58@users.noreply.github.com>
2021-04-08 23:29:43 +0800

    Update Time Converter.py
    Coding style is important

--- Time Converter.py
+++ Time Converter.py
@@ -2,7 +2,7 @@ print("This program will convert seconds to hours, minutes and seconds.")
 
 def main():
 # input time
-    time=int(input("Please input the seconds.\n"))
+    time = int(input("Please input the seconds.\n"))
 
 # converter
     Second = time % 60
@@ -10,9 +10,9 @@ def main():
     Hour = ((time - Second) / 60 ) / 60
 
 # output time format
-    h="%02d" % Hour
-    m="%02d" % Minute
-    s="%02d" % Second
+    h = "%02d" % Hour
+    m = "%02d" % Minute
+    s = "%02d" % Second
     print(h,":",m,":",s)
 
 # exception detect
